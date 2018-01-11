ab4ba274a22f34d6028fcd9c802ec622f14c1cc1
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-01-11 11:45:06 +0100

    extra installation script

---
+++ misc/jup_rise_install.sh
@@ -0,0 +1,5 @@
+sudo python3 -m pip install jupyter
+sudo python3 -m pip install RISE
+sudo jupyter-nbextension install rise --py --sys-prefix
+sudo jupyter-nbextension enable rise --py --sys-prefix
+
