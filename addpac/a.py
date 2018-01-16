751640429a6b018bcbb797029b8abddc0d9c3c28
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-01-16 11:23:47 +0100

    Add more to the sklearn pipelines tutorial

--- addpac/a.py
+++
@@ -1,7 +0,0 @@
-def sumfnc(a, b):
-    return a+b
-
-class A():
-    pass
-
-VARIABLE = "Hello code sharing!"
--- b.py
+++
@@ -1,7 +0,0 @@
-from addpac.a import sumfnc
-
-class A():
-    def _fnc(self):
-        return None
-
-print(sumfnc(2,2))
